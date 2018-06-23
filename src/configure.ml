2f5c6c55da70a4aaec73fc1132a7223ee73760c7
Anil Madhavapeddy <anil@recoil.org>
2018-06-23 12:20:00 +0100

    Import jbuild support from ocaml/opam@c35eb977d19457d3
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- src/configure.ml
+++ src/configure.ml
@@ -7,9 +7,11 @@ let () =
   match Sys.argv with
   | [|_;"-cppo-args"|] ->
     let version = Scanf.sscanf Sys.ocaml_version "%d.%d." (fun major minor -> major * 100 + minor) in
-    printf "-D \\\"OCAML %d\\\"\n" version;
-    print_endline (if Sys.word_size = 32 then "-D WORD_SIZE_32 " else "");
-    show_bytes "-D WITH_BYTES";
+    printf "-D\n";
+    printf "OCAML %d\n" version;
+    if Sys.word_size = 32 then (print_endline "-D"; print_endline "WORD_SIZE_32 ");
+    print_endline "-D";
+    show_bytes "WITH_BYTES";
     exit 0
   | [|_;"-compile-args"|] ->
     if Sys.ocaml_version >= "4.00.0" then print_endline "-bin-annot";
